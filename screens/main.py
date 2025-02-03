6b669fca8b3f6ba637013ed0a76a3a356c497f3b
Hender Hernandez Caba <henderhernandezc1@gmail.com>
2025-02-03 12:34:53 -0500

    added code

--- screens/main.py
+++ screens/main.py
@@ -2,6 +2,8 @@ import color
 import speed
 import title
 import difficulty  # Import your information_screen function
+import game_info
+import ending_credits
 
 def main():
     colorFunc = color.choose_color_mode()
@@ -17,7 +19,12 @@ def main():
     player_level = difficulty.information_screen()  # Call the function and store the result
     if player_level is None:
         return  # Exit if user quits
-
+    
+    infoFunc = game_info.info_screen1() # General info, displayes after choosing the difficulty
+    
+    
+    endingFunc = ending_credits.ending_creds() # ENDING CREDITS, this function is placed here temporarily
+ 
 if __name__ == "__main__":
     main()
 
