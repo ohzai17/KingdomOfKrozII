20673c7a444d5971479d9a7c67b083e2eb9ecbf9
Au Sein <seina@sunypoly.edu>
2025-02-24 12:55:16 -0500

    folder sort

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from title_screens import *
-from levels.gameplay import *
+# from levels.gameplay import *
 
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((832, 624))
     run_all_title_screens(screen)
     # Removed level from main, now it's only called when user inputs "b" in load() function.
-    ending_creds(screen) # Ending credits
+    # ending_creds(screen) # Ending credits
     pygame.quit()
