ed21fcc1783a6269bc7fca19f0c21a2234f6f8c3
Au Sein <seina@sunypoly.edu>
2025-03-17 12:26:22 -0400

    added mixer line to main

--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ from screens import *
 if __name__ == "__main__":
 
     pygame.init()
-<<<<<<< HEAD
     pygame.mixer.init()
-=======
->>>>>>> 55b80a57edd6b6c2126747f03c98113db20fd74a
     screen = pygame.display.set_mode((832, 624))
     pygame.display.set_caption("Kingdom of Kroz II")
     run_all_screens(screen)
