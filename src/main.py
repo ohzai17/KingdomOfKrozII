efc0d5d7fb69b7bbca3fe07e15be3024b5a7bf30
Edward Campion III <campioes@sunypoly.edu>
2025-04-14 11:34:24 -0400

    Full screens update (bar monochrome not working bug)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from screens import *
+from utils import screen, pygame
+from screens import run_all_screens, sign_off
+from gameplay import levels
 
 if __name__ == "__main__":
 
     run_all_screens(screen)
+    levels(screen)
     sign_off(screen)
     pygame.quit()
